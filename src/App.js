ea1baeb70253ac1c0ae395f7359d4995c89820df
a-gribov <llununu@gmail.com>
2020-10-06 13:47:29 +0300

    app.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Header />
         <p>
-          This is my first React.js project <code>src/App.js</code> Gribov Andrey
+          This is my first React.js project. Gribov Andrey
         </p>
         <a
           className="App-link"
@@ -21,6 +23,16 @@ function App() {
       </header>
     </div>
   );
+}
+const Header = () => {
+  return (
+      <div className="header">
+        <a>css</a>
+        <a>html</a>
+        <a>js</a>
+        <a>react.js</a>
+      </div>
+  )
 }
 
 export default App;
