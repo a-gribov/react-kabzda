a5af64b1280ff99bc6448190584e831e9f76a431
a-gribov <llununu@gmail.com>
2020-10-06 13:00:24 +0300

    app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is my first React.js project <code>src/App.js</code> Gribov Andrey.
+          This is my first React.js project <code>src/App.js</code> Gribov Andrey
         </p>
         <a
           className="App-link"
